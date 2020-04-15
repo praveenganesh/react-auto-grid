d2eefccd5b67dc5b41c1f66fe83aa9054a5b75d3
pk <praveeng@arkainsure.com>
2020-04-15 16:51:25 +0530

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-{ 
-    test: /\.jsx?$/,         // Match both .js and .jsx files
-    exclude: /node_modules/, 
-    loader: "babel", 
-    query:
-      {
-        presets:['react']
-      }
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    }
 }
